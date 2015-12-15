396599f6fddc027c282487a727c4029cd6f3f012
Teagan Wrest <wrestteagan@gmail.com>
2015-12-15 12:20:15 -0800

    added query for light data

--- collectData.py
+++ collectData.py
@@ -10,3 +10,17 @@ PASSWORD = plantDataPassword
 conn = psycopg2.connect(database="plantdata", user=USERNAME, password=PASSWORD, host="127.0.0.1", port="5432")
 cur = conn.cursor();
 print "Opened database successfully"
+
+cur.execute("SELECT id, my_date, idpi, ir, lux, visible FROM light")
+rows = cur.fetchall()
+for row in rows:
+   print "ID = ", row[0]
+   print "date = ", row[1]
+   print "idpi = ", row[2]
+   print "ir = ", row[3]
+   print "lux = ", row[4]
+   print "visible = ", row[5], "\n"
+
+print "Operation done successfully"
+conn.close();
+sys.exit();
