a0e0bf594146b3b2d047bb8ce35ba2741f33cc5d
Teagan Wrest <wrestteagan@gmail.com>
2015-12-15 13:05:47 -0800

    added arduino code

--- arduino.ino
+++ arduino.ino
@@ -5,3 +5,128 @@
 #include "DHT.h"
 #define DHTPIN 2
 #define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+Adafruit_TSL2591 tsl = Adafruit_TSL2591(2591);
+LiquidCrystal_I2C lcd(0x27,20,4);
+byte newChar[8] = {
+  B00011,
+  B00011,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+};
+
+int buttonPin = 8;
+int pumpPin = 11;
+int count = 0;
+int soilHumidity;
+int waterButton;
+int screenCount;
+float fahrenheit;
+float humidity;
+uint32_t lum;
+uint16_t irLight, fullLight, luxLight, visibleLight;
+
+void configure(void) {
+  lcd.init();
+  lcd.home();
+  lcd.begin(20, 4);
+  lcd.createChar(0, newChar);
+  tsl.setGain(TSL2591_GAIN_MED);
+  tsl.setTiming(TSL2591_INTEGRATIONTIME_400MS);
+  pinMode(pumpPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+}
+
+void basicLcdPrint(void) {
+  lcd.setCursor (0,0);
+  lcd.print("Lux: " + String(luxLight));
+  lcd.setCursor(0, 1);
+  lcd.print("Air Temp: " + String(fahrenheit));
+  lcd.write(byte(0));
+  lcd.print("F");
+  lcd.setCursor(0,2);
+  lcd.print("Air Humidity: ");
+  lcd.print(String(humidity) + "%");
+  lcd.setCursor(0,3);
+  lcd.print("Soil Humidity: ");
+  lcd.print(String(soilHumidity) + "%");
+}
+
+void wateringLcdPrint(void) {
+  lcd.clear();
+  lcd.setCursor(6,1);
+  lcd.print("Watering");
+}
+
+void lowWaterLcdPrint(void) {
+  lcd.clear();
+  lcd.setCursor(5,1);
+  lcd.print("Low Water");
+}
+
+void setup(void) {
+  Serial.begin(9600);
+  if (!tsl.begin()) {
+    Serial.println("No sensor found ... check your wiring?\n");
+  } else {
+    configure();
+  }
+}
+
+void basicRead(void) {
+  Serial.println(String(waterButton) + ',' + String(irLight) + ',' + String(visibleLight) + ',' + String(luxLight) + ',' + String(soilHumidity) + '\n');
+}
+
+void advancedRead(void) {
+  humidity = dht.readHumidity();
+  fahrenheit = dht.readTemperature(true);
+  if (isnan(humidity) || isnan(fahrenheit)) {
+    Serial.println("Failed to read from DHT sensor!\n");
+    return;
+  } else  {
+    basicLcdPrint();
+    Serial.print(String(waterButton) + ',' + String(irLight) + ',' + String(visibleLight) + ',' + String(luxLight) + ',' + String(soilHumidity) + ',' + String(fahrenheit) + ',' + String(humidity) + '\n');
+  }
+}
+
+void loop(void) {
+  soilHumidity = word(analogRead(A0));
+  waterButton = digitalRead(buttonPin);
+  Serial.print(String(waterButton));
+  lum = tsl.getFullLuminosity();
+  irLight = lum >> 16;
+  fullLight = lum & 0xFFFF;
+  visibleLight = fullLight - irLight;
+  luxLight = tsl.calculateLux(fullLight, irLight);
+  if (waterButton == 1) {
+    if(soilHumidity > 0) {
+      digitalWrite(pumpPin, HIGH);
+      wateringLcdPrint();
+    } else {
+      digitalWrite(pumpPin, LOW);
+      lowWaterLcdPrint();
+    }
+    lcd.backlight();
+    screenCount = 20;
+  } else {
+    digitalWrite(pumpPin, LOW);
+    if(screenCount > 0) {
+      lcd.backlight();
+      screenCount -= 1;
+    } else {
+      lcd.noBacklight();
+    }
+    if (count < 10) {
+      basicRead();
+      count += 1;
+    } else {
+      count = 0;
+      advancedRead();
+    }
+  }
+  delay(50);
+}
