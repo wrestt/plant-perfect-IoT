f5790a9db4acf10ead1127982ec76c5412cc69b7
Teagan Wrest <wrestteagan@gmail.com>
2015-12-15 12:39:37 -0800

    added postgres commands for creating datable and tables

---
+++ postgresqlCommands.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE plantdata;
+
+CREATE TABLE pi (
+	  id SERIAL PRIMARY KEY,
+	  idpi TEXT
+);
+
+CREATE TABLE light (
+	  id SERIAL PRIMARY KEY,
+	  my_date TIMESTAMP default current_timestamp,
+	  idpi TEXT,
+	  ir NUMERIC,
+	  lux NUMERIC,
+	  visible NUMERIC
+);
+
+CREATE TABLE air (
+	  id SERIAL PRIMARY KEY,
+	  my_date TIMESTAMP default current_timestamp,
+	  idpi TEXT,
+	  airtemp NUMERIC,
+	  airhumidity NUMERIC
+);
+
+CREATE TABLE soil (
+	  id SERIAL PRIMARY KEY,
+	  my_date TIMESTAMP default current_timestamp,
+	  idpi TEXT,
+	  soilhumidity NUMERIC
+);
+
