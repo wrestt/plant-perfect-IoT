fb338d762686f0a107457b9d0489b89d3dd26e19
Teagan Wrest <wrestteagan@gmail.com>
2015-12-16 01:53:33 -0800

    fixed semi typo

--- models/index.js
+++ models/index.js
@@ -2,6 +2,6 @@ var bookshelf = require();
 
 module.exports.Air = require('./air');
 module.exports.Light = require('./Light');
-module.exports.Pi = require('./Pi';)
+module.exports.Pi = require('./Pi');
 module.exports.Soil = require('./Soil');
 module.exports.Water = require('./Water');
