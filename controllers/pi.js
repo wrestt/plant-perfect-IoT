48ef70b56166a04ccc5ad7f2070bd45c65170e29
Teagan Wrest <wrestteagan@gmail.com>
2015-12-15 23:20:58 -0800

    fetch pi by id closes #12

--- controllers/pi.js
+++ controllers/pi.js
@@ -1,5 +1,15 @@
 var Model = require('./../models/Pi');
 
+var getPi = function(req, res) {
+  var piId = req.params.id;
+  new Model.Pi().where('idpi', piId).fetch()
+    .then(function(pi) {
+      res.json(pi);
+    }).catch(function(error) {
+      res.json(error);
+    });
+};
+
 var getAllPis = function(req, res) {
   new Model.Pi().fetchAll()
     .then(function(pis) {
@@ -7,4 +17,9 @@ var getAllPis = function(req, res) {
     }).catch(function(error) {
       res.json(error);
     });
+};
+
+module.exports = {
+  getPi: getPi,
+  getAllPis: getAllPis
 };
