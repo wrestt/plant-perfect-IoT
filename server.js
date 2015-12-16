e1dcf1d5f38bceb86208d0f5cf68c2adf2cda549
Teagan Wrest <wrestteagan@gmail.com>
2015-12-15 22:05:31 -0800

    connected bookshelf and knex to database

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 var express = require('express');
-var bodyparser = require('bodyparser');
+var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var session = require('cookie-session');
+var config = require('./env.json')[process.env.NODE_ENV || 'production'];
+var dbUsername = config.PLANTDATA_USERNAME;
+var dbPassword = config.PLANTDATA_PASSWORD;
+var knex = require('knex')({
+  client: 'pg',
+  connection: 'postgres://' + dbUsername + ':' + dbPassword + '@localhost:5432/plantdata',
+  searchPath: 'knex,public'
+});
+var bookshelf = require('bookshelf')(knex);
 
+require('./controllers/index');
+
+apiRouter = express.Router();
+app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));
+app.use(express.static('public'));
+app.use(express.static(__dirname + '/app'));
 
 app.get('*', function(req, res) {
   res.render('index.html.ejs');
