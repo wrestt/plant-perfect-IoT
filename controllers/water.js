2d8093efdb5dd1e43f23824c27470fb5e3557fe3
Teagan Wrest <wrestteagan@gmail.com>
2015-12-16 01:13:22 -0800

    added water controller closes #7

---
+++ controllers/water.js
@@ -0,0 +1,25 @@
+var Model = require('./../models/Water');
+
+var getWAter = function(req, res) {
+  var piId = req.params.id;
+  new Model.Water().where('idpi', piId).fetch()
+    .then(function(pi) {
+      res.json(pi);
+    }).catch(function(error) {
+      res.json(error);
+    });
+};
+
+var getAllWaters = function(req, res) {
+  new Model.Water().fetchAll()
+    .then(function(pis) {
+      res.json(pis);
+    }).catch(function(error) {
+      res.json(error);
+    });
+};
+
+module.exports = {
+  getWater: getWater,
+  getAllWaters: getAllWaters
+};
