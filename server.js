6a3605df3ff7524c4236f786e57f352d290e3e54
Teagan Wrest <wrestteagan@gmail.com>
2015-12-15 22:33:10 -0800

    change location of bookshelf/knex to config/db

--- server.js
+++ server.js
@@ -2,17 +2,6 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var session = require('cookie-session');
-var config = require('./env.json')[process.env.NODE_ENV || 'production'];
-var dbUsername = config.PLANTDATA_USERNAME;
-var dbPassword = config.PLANTDATA_PASSWORD;
-var knex = require('knex')({
-  client: 'pg',
-  connection: 'postgres://' + dbUsername + ':' + dbPassword + '@localhost:5432/plantdata',
-  searchPath: 'knex,public'
-});
-var bookshelf = require('bookshelf')(knex);
-
-require('./controllers/index');
 
 apiRouter = express.Router();
 app = express();
@@ -26,6 +15,8 @@ app.use(express.static(__dirname + '/app'));
 app.get('*', function(req, res) {
   res.render('index.html.ejs');
 });
+
+require('./controllers/index');
 
 app.listen(process.env.PORT || 3000, function() {
   console.log('server is listening on port ' + process.env.PORT || 3000);
