9384f1d36609f2b7a83616adf4a2b8d87f0269f4
Teagan Wrest <wrestteagan@gmail.com>
2015-12-16 01:41:49 -0800

    added bookshelf one to many closes #5

--- controllers/pi.js
+++ controllers/pi.js
@@ -3,20 +3,26 @@ var Model = require('./../models/Pi');
 var getPi = function(req, res) {
   var piId = req.params.id;
   new Model.Pi().where('idpi', piId).fetch()
-    .then(function(pi) {
-      res.json(pi);
-    }).catch(function(error) {
-      res.json(error);
+  .fetch({withRelated: ['light', 'soil', 'water', 'air']})
+  .then(function(pi) {
+    if (!pi) {
+      res.status(404).json({error: true, data: {}});
+    } else {
+      res.json({error: false, data: pi.toJSON()});
+    }
+  }).catch(function(err) {
+      res.status(500).json({error: true, data: {message: err.message}});
     });
 };
 
 var getAllPis = function(req, res) {
   new Model.Pi().fetchAll()
-    .then(function(pis) {
-      res.json(pis);
-    }).catch(function(error) {
-      res.json(error);
-    });
+  .then(function(pis) {
+    res.json({error: false, data: pis.toJSON()});
+  })
+  .catch(function(err) {
+    res.status(500).json({error: true, data: {message: err.message}});
+  });
 };
 
 module.exports = {
--- models/Pi.js
+++ models/Pi.js
@@ -1,7 +1,24 @@
 var bookshelf = require('./../config/db').bookshelf;
 
 var Pi = bookshelf.Model.extend({
-  tableName: 'pi'
+  tableName: 'pi',
+
+  airs: function() {
+    return this.hasMany(Air, 'idpi');
+  },
+
+  soils: function() {
+    return this.hasMany(Soil, 'idpi');
+  },
+
+  waters: function() {
+    return this.hasMany(Water, 'idpi');
+  },
+
+  lights: function() {
+    return this.hasMany(Light, 'idpi');
+  }
+
 });
 
 module.exports = {
