388bf0bab86fb4256ba3d328e21115dd1ea88c38
Teagan Wrest <wrestteagan@gmail.com>
2015-12-16 16:07:25 -0800

    change naming

--- collectData.py
+++ collectData.py
@@ -15,35 +15,35 @@ print "Opened database successfully"
 
 def createPiRecords(piId):
     try:
-        cur.execute("INSERT INTO pi (idpi) VALUES ('%s')" % (piId))
+        cur.execute("INSERT INTO pi (pi_id) VALUES ('%s')" % (piId))
         print "Pi records created successfully"
     except:
         print "Pi Record already exists"
     conn.commit();
 
 def createWaterRecords(piId, data):
-    cur.execute("INSERT INTO water (idpi, watering, available ) VALUES ('%s', '%s', '%s')" % \
+    cur.execute("INSERT INTO water (pi_id, watering, available ) VALUES ('%s', '%s', '%s')" % \
         (piId, data[0], data[1])
     )
     conn.commit();
     print "Water records created successfully"
 
 def createLightRecords(piId, data):
-    cur.execute("INSERT INTO light (idpi, ir, lux, visible) VALUES ('%s', '%s', '%s', '%s')" % \
+    cur.execute("INSERT INTO light (pi_id, ir, lux, visible) VALUES ('%s', '%s', '%s', '%s')" % \
         (piId, data[2], data[3], data[4])
     )
     conn.commit();
     print "Light records created successfully"
 
 def createSoilRecords(piId, data):
-    cur.execute("INSERT INTO soil(idpi, humidity) VALUES ('%s', '%s')" %\
+    cur.execute("INSERT INTO soil(pi_id, humidity) VALUES ('%s', '%s')" %\
         (piId, data[5])
     )
     conn.commit();
     print "Soil records created successfully"
 
 def createAirRecords(piId, data):
-    cur.execute("INSERT INTO air(idpi, temp, humidity) VALUES ('%s', '%s', '%s')" % \
+    cur.execute("INSERT INTO air(pi_id, temp, humidity) VALUES ('%s', '%s', '%s')" % \
         (piId, data[6], data[7])
     )
     conn.commit();
@@ -70,39 +70,39 @@ while True:
         print data
 
 
-cur.execute("SELECT id, created_at, idpi, watering, available FROM water")
+cur.execute("SELECT id, created_at, pi_id, watering, available FROM water")
 rows = cur.fetchall()
 for row in rows:
     print "ID = ", row[0]
     print "date = ", row[1]
-    print "idpi = ", row[2]
+    print "pi_id = ", row[2]
     print "watering = ", row[3]
     print "present = ", row[4]
 
-cur.execute("SELECT id, created_at, idpi, ir, lux, visible FROM light")
+cur.execute("SELECT id, created_at, pi_id, ir, lux, visible FROM light")
 rows = cur.fetchall()
 for row in rows:
    print "ID = ", row[0]
    print "date = ", row[1]
-   print "idpi = ", row[2]
+   print "pi_id = ", row[2]
    print "ir = ", row[3]
    print "lux = ", row[4]
    print "visible = ", row[5], "\n"
 
-cur.execute("SELECT id, created_at, idpi, humidity FROM soil")
+cur.execute("SELECT id, created_at, pi_id, humidity FROM soil")
 rows = cur.fetchall()
 for row in rows:
    print "ID = ", row[0]
    print "date = ", row[1]
-   print "idpi = ", row[2]
+   print "pi_id = ", row[2]
    print "soil = ", row[3], "\n"
 
-cur.execute("SELECT id, created_at, idpi, temp, humidity FROM air")
+cur.execute("SELECT id, created_at, pi_id, temp, humidity FROM air")
 rows = cur.fetchall()
 for row in rows:
     print "ID = ", row[0]
     print "date = ", row[1]
-    print "idpi = ",
+    print "pi_id = ",
     print "temp = ", row[3]
     print "humidity = ", row[4], "\n"
 
