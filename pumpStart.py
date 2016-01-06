9b791b66f5e5975e15b2dca369436feafbd5df99
Teagan Wrest <wrestteagan@gmail.com>
2016-01-06 02:26:22 -0800

    change python logic

--- pumpStart.py
+++ pumpStart.py
@@ -1,14 +1,28 @@
-import RPi.GPIO as GPIO
-from time import sleep
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(8, GPIO.OUT)
+import RPi.GPIO as GPIO            # import RPi.GPIO module
+from time import sleep             # lets us have a delay
+GPIO.setmode(GPIO.BCM)             # choose BCM or BOARD
+GPIO.setup(24, GPIO.OUT)           # set GPIO24 as an output
 
 try:
-    GPIO.output(8, 1)
-    print "high"
+    while True:
+        GPIO.output(24, 1)
 except KeyboardInterrupt:
-    print "\n Stopped by Keyboard"
-except:
-    print "Error occurred with setting GPIO pin to high"
-finally:
     GPIO.cleanup()
+
+
+
+
+# import RPi.GPIO as GPIO
+# from time import sleep
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setup(8, GPIO.OUT)
+#
+# try:
+#     GPIO.output(8, 1)
+#     print "high"
+# except KeyboardInterrupt:
+#     print "\n Stopped by Keyboard"
+# except:
+#     print "Error occurred with setting GPIO pin to high"
+# finally:
+#     GPIO.cleanup()
