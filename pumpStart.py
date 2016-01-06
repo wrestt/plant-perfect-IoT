6609dfc46bca52211964f10b8598efdc7500acc3
Teagan Wrest <wrestteagan@gmail.com>
2016-01-06 02:45:24 -0800

    changing gpio logic for testing

--- pumpStart.py
+++ pumpStart.py
@@ -3,16 +3,6 @@ from time import sleep             # lets us have a delay
 GPIO.setmode(GPIO.BCM)             # choose BCM or BOARD
 GPIO.setup(24, GPIO.OUT)           # set GPIO24 as an output
 
-# try:
-#     while True:
-#         GPIO.output(24, 1)         # set GPIO24 to 1/GPIO.HIGH/True
-#         sleep(5)                 # wait half a second
-#         GPIO.output(24, 0)         # set GPIO24 to 0/GPIO.LOW/False
-#         sleep(5)                 # wait half a second
-#
-# except KeyboardInterrupt:          # trap a CTRL+C keyboard interrupt
-#     GPIO.cleanup()                 # resets all GPIO ports used by this program
-
 try:
     GPIO.output(24, 1)
     print "high"
--- pumpStop.py
+++ pumpStop.py
@@ -1,13 +1,13 @@
-import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(8, GPIO.OUT)
+import RPi.GPIO as GPIO            # import RPi.GPIO module
+from time import sleep             # lets us have a delay
+GPIO.setmode(GPIO.BCM)             # choose BCM or BOARD
+GPIO.setup(24, GPIO.OUT)           # set GPIO24 as an output
+
 
 try:
-    GPIO.output(8, 0)
+    GPIO.output(24, 0)
     print "low"
 except KeyboardInterrupt:
     print "\n Ending"
 except:
     print "Error occurred with setting GPIO pin to low"
-finally:
-    GPIO.cleanup()
